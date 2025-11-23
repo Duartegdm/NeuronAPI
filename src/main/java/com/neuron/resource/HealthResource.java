2e00aeb246b168dcfea95327be95f5c21db061c3
duartegdm <gduarte.study@gmail.com>
2025-11-23 17:35:59 -0300

    Adicionando health resource

---
+++ src/main/java/com/neuron/resource/HealthResource.java
@@ -0,0 +1,19 @@
+package com.neuron.resource;
+
+import jakarta.annotation.security.PermitAll;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import java.time.Instant;
+
+@Path("/health")
+@PermitAll
+public class HealthResource {
+
+    @GET
+    public Response health() {
+        return Response.ok().entity("{\"status\": \"OK\", \"timestamp\": \"" +
+                Instant.now().toString() + "\"}").build();
+    }
+}
